c4f46fd3a081e4bdc7dd77918dd329e750a169ef
Cody Thaller <stevencthaller@gmail.com>
2020-08-31 11:33:30 -0700

    Lecture on Hoist/Scope and Destructuring

--- Fundamentals/destructuring_intro.js
+++ Fundamentals/destructuring_intro.js
@@ -7,3 +7,77 @@ What are some use cases?
 
 How do??
 */
+
+
+let me = {
+    firstName: "Steven",
+    middleName: "Cody",
+    lastName: "Thaller",
+    age: 29,
+    height: `5'11"`,
+    ssn: 123121234,
+    ccn: 1234123456785678
+};
+
+/* ********************** */
+// If you want to re-assign the values to new variable names
+let { firstName: steve } = me;
+
+console.log(steve);
+/* *********************** */
+
+// This is the old way, and it's gross. 
+// let newMe = {}
+
+// newMe.firstName = me.firstName;
+// newMe.middleName = me.middleName;
+// newMe.lastName = me.lastName;
+// newMe.age = me.age;
+// newMe.height = me.height;
+
+
+// OR we can destructure with a spread operator
+let { ssn, ccn, ...newMe } = me;
+
+console.log(me);
+console.log(newMe);
+
+// OLD WAY
+// let firstName = me.firstName;
+// let lastName = me.lastName;
+
+//NEW WAY
+let { firstName, lastName } = me;
+
+let myArray = [1,3,true,"toes",-2]
+let [...newarr] = myArray;
+
+// OLD WAY
+// let first = myArray[0];
+// let weird = myArray[3];
+// let neg = myArray[4];
+
+let [first,,,weird,neg] = myArray;
+
+/* ***************************************** */
+
+let joe = {
+    first: "Joe",
+    last: "Smith"
+}
+let jane = {
+    first: "Jane",
+    last: "Doe"
+}
+let bill = {
+    first: "Bill",
+    last: "Gates"
+}
+
+let peeps = [joe,jane,bill];
+
+let [one, two, three] = peeps;
+
+let { jo } = one;
+let { ja } = two;
+let { bi } = three;
--- Fundamentals/hoisting_intro.js
+++ Fundamentals/hoisting_intro.js
@@ -3,8 +3,67 @@ What is hoisting?
 
 What's so important about it?
 
-Is this something I need to consciously implement into my code?
+Is this something I need to consciously implement into my code? - not really
 
 var vs let vs const ?????
 */
 
+
+// implicit variable declaration for my_name prior to its actual declaration
+// console.log(my_name);
+
+// var my_name = "Cody"
+// let my_name2 = "Thaller";
+
+// const person = {
+//     name: "Cody",
+//     age:29
+// };
+
+// console.log(my_name);
+// console.log(my_name2);
+// my_name2 = "Bananas";
+// console.log(my_name2);
+
+// console.log(person);
+// person.name = "Steven";
+// console.log(person);
+
+var y = 25;
+bubble();
+// bubble2();
+console.log(y);
+// console.log(x); // THIS WON'T WORK
+
+function bubble(){
+    var x = 10
+    console.log(y);
+    y += x;
+}
+
+const bubble2 = () => {
+    console.log(10);
+}
+
+bubble2();
+
+// var -> hoists the variable NAME but not its value to the top of the scope it was declared in
+// let -> DOES NOT HOIST
+// const -> DOES NOT HOIST
+
+for(var i = 0; i < 10; i++){
+    console.log(i*3);
+}
+
+console.log(i);
+let x = true;
+let count = 0;
+while(x == true){
+    let z = 10;
+    if(count == 3){
+        x = false;
+    }
+    count++;
+}
+
+console.log(z);
