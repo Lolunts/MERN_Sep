a0af7ab1e23df7bb74908977e9c3dc2a61a5c962
Cody Thaller <stevencthaller@gmail.com>
2020-09-02 11:25:28 -0700

    React Intro

---
+++ React/ReactIntro/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <>
+      <h1>Welcome to my site</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa a, quisquam accusamus inventore id iste laudantium placeat, natus molestiae pariatur ducimus deserunt. Eum omnis adipisci temporibus soluta, accusamus explicabo laboriosam?</p>
+    </>
+  );
+}
+
+export default App;
