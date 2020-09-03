3a33ff707b16a50acfeed0cc2c90f1b54a7729da
Cody Thaller <stevencthaller@gmail.com>
2020-09-03 08:43:08 -0700

    Merge Sort

---
+++ Algos/merge_sort.js
@@ -0,0 +1,16 @@
+// This is going to be a 2 parter.
+
+// Part 1: Write a function that will take 2 sorted arrays, and merge them together.
+// Don't worry about doing it in place. Just create a new array and combine the 2 parameter
+// arrays
+function merge(arr1, arr2){
+
+}
+
+// Part 2: Write a function that performs merge sort. 
+// This will involve 2 key points:
+//  1. Recursion
+//  2. The previously defined merge function
+function mergeSort(arr){
+
+}
