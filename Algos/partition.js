72d49a3cb73c64a39823c4986d0a4ad74c40adaf
Cody Thaller <stevencthaller@gmail.com>
2020-09-04 08:49:02 -0700

    Partition

---
+++ Algos/partition.js
@@ -0,0 +1,12 @@
+// Write a function that partitions an array. What I mean by this, is
+// take the right-most index, and re-arrange the elements in the array so that the 
+// number was at the right-most index is in a position such that everything
+// to the left of it is less than it, and everything to the right of it is
+// greater than it, and return the new index of that element
+
+// EXAMPLE: let arr = [9,2,45,3,12]
+// partition(arr) returns 3, and arr is now
+// [9,2,3,12,45]
+function partition(arr = [], left = 0, right = arr.length-1){
+    
+}
