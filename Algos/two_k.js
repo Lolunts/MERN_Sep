d9efe028c7b3b9f3f3981a50596ee37f58c4e927
Cody Thaller <stevencthaller@gmail.com>
2020-09-15 08:47:06 -0700

    Added algo prompt 9/15

---
+++ Algos/two_k.js
@@ -0,0 +1,24 @@
+// TWO ALGOS TODAY WOOOOOOO
+
+// Algo 1
+// Given an array of integers, return indices of the two numbers such that the 
+// elements at those indices add up to a specific target. You may not use the same element 
+// twice
+
+// Bonus if you can make it O(n) time complexity
+
+// EXAMPLE: twoSum([2,11,7,15], 9) should return [0,2] because
+// 2 + 7 = 9
+function twoSum(arr, target){
+
+}
+
+// Algo 2
+// Given an unsorted array of integers, and a number, k, return the k most
+// frequent elements in any order.
+
+// EXAMPLE: (note, this solution is sorted for readability, it is not expected that you sort the response)
+// kMostFrequent([1,1,1,2,2,3], 2) should return [1,2] because 1 is the most frequent, 2 is the 2nd most frequent,
+function kMostFrequent(arr, k){
+
+}
