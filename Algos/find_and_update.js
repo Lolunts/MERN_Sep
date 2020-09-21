33039c5106d82653908617bca837983ed349141b
Cody Thaller <stevencthaller@gmail.com>
2020-09-21 08:56:58 -0700

    Algo promtpts 9/21

---
+++ Algos/find_and_update.js
@@ -0,0 +1,69 @@
+// TWO ALGOS WOOOO
+// Algo 1:
+
+// Write a function that takes a list of objects, and a criteria object, searches through the list and returns
+// any objects that meet the criteria.
+
+// EXAMPLE:
+// const items = [
+//     { firstName: "Bob", lastName: "Bobbert", age: 31 },
+//     { firstName: "John", lastName: "Smith", age: 25 },
+//     { firstName: "Bob", lastName: "Smith", age: 27 },
+//     { firstName: "Bob", lastName: "White", age: 31 },
+//   ];
+// const searchCriteria = {
+//     firstName: "Bob",
+//     age: 31,
+//   };
+// findObjects(items, searchCriteria)
+// should return the first and last objects in the items list
+function findObjects(items, criteria){
+
+}
+
+
+// ALGO 2:
+
+// This function is similar to the previous one, but rather than just find one,
+// find it and update it.
+// The function that takes an id, an object with key value pairs (update values), and a list of
+// objects (collection), and returns the updated.
+
+// EXAMPLE:
+// const students = [
+//     {
+//       id: 1,
+//       name: "student1",
+//       isLateToday: false,
+//       lateCount: 15,
+//       redBeltStatus: false,
+//     },
+//     {
+//       id: 2,
+//       name: "student2",
+//       isLateToday: false,
+//       lateCount: 1,
+//       redBeltStatus: false,
+//     },
+//     {
+//       id: 3,
+//       name: "student3",
+//       isLateToday: false,
+//       lateCount: 0,
+//       redBeltStatus: false,
+//     },
+//   ];
+  
+//   const id1 = 3;
+//   const updateData1 = { redBeltStatus: true, isLateToday: true };
+// findByIdAndUpdate(id1, updateData1, students) should return
+// {
+//     id: 3,
+//     name: "student3",
+//     isLateToday: true,
+//     lateCount: 0,
+//     redBeltStatus: true,
+// }
+function findByIdAndUpdate(id, updateValues, collection){
+    
+}
