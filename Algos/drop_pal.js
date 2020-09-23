19721b3e4f6fbfbce70ccd9009ebbe4eb3c92599
Cody Thaller <stevencthaller@gmail.com>
2020-09-23 08:37:36 -0700

    Algos 9/23

---
+++ Algos/drop_pal.js
@@ -0,0 +1,23 @@
+// TWO ALGOS WOOOOOO
+
+// Algo 1 - Drop It
+// Write a function that takes 2 parameters: an array, and a callback function.
+// The goal is to make it so this function will remove elements from the array (starting at index zero)
+// for every element that the callback function returns true for.
+function dropIt(arr, callback){
+
+}
+
+// Algo 2 - Can it become a palindrome?
+
+// Write a function that takes a string as a parameter, and determines whether
+// or not the characters of the string could potentially be rearranged to create a 
+// palindrome.
+
+// EXAMPLE:
+// canPali("toccata") would return true, because the letters could be rearranged to 
+// make it "tacocat", but canPali("green") would return false because there can't be 
+// a palindrome from that.
+function canPali(string){
+    
+}
