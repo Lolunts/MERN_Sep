ffd8f9e31319e8f571bd36d8a05e9901229f9bfc
Cody Thaller <stevencthaller@gmail.com>
2020-09-24 08:58:31 -0700

    Algo prompt 9/24

---
+++ Algos/hon_vNum.js
@@ -0,0 +1,19 @@
+// TWO ALGOS
+
+// Algo 1
+// Honorifics -> write a function that takes a string (something like "Mr", "Mrs", "Sir"),
+// and an array of strings that make a full name, and returns the honorific version
+// EXAMPLE: honorific("Mr", ["John", "Jacob", "Jingleheimer", "Schmidt"]) returns 
+// "Mr John Jacob Jingleheimer Schmidt"
+function honorific(honorofic, fullName){
+
+}
+
+// Algo 2
+// Compare Version Numbers -> Write a function that takes two strings, version1 and version2, and 
+// determine which version number is later. 
+
+// EXAMPLE: compareVersionNumbers("1.0.1", "1.1") should return "1.1" since v1.1 is greater than v1.0.1
+function compareVersionNumbers(version1, version2){
+    
+}
